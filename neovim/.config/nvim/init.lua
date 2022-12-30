947191a1d62a0d4992486097d9a37137aa87ec9a
Matthew Hanna <mattnhanna@protonmail.com>
2022-12-30 10:13:02 -0700

    Install packer.nvim if not already installed

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,7 +1,25 @@
+--install packer.nvim if it's not already installed (taken from the Github README, credit to @Iron-E and @khuedoan there)
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 --package management
 require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'morhetz/gruvbox'
+	--if packer wasn't installed install all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
 
 --color settings
