93ce750a6ce2ed40cb43cf52e436b9d42b64303c
Matthew Hanna <MattNHanna@protonmail.com>
2025-12-14 22:17:51 -0700

    Replace old neovim config
    Still left the old config in there, and also forgot that this was
    supposed to automatically put things in $HOME

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,57 +1,31 @@
---install packer.nvim if it's not already installed (taken from the Github README, credit to @Iron-E and @khuedoan there)
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
+-- plugin manager
+require("config.lazy")
 
-local packer_bootstrap = ensure_packer()
+--colors
+vim.o.termguicolors = true
+vim.o.background = "dark"
+vim.cmd("colorscheme gruvbox")
 
---package management
-require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'morhetz/gruvbox'
-        use 'neovim/nvim-lspconfig'
-        use {
-            'ms-jpq/coq_nvim', branch = 'coq', run = 'python3 -m coq deps'
-        }
-        use {
-            'ms-jpq/coq.artifacts', branch = 'artifacts'
-        }
-        use {
-            'ms-jpq/coq.thirdparty', branch = '3p'
-        }
-        use 'yuezk/vim-js'
-        use 'maxmellon/vim-jsx-pretty'
-	--if packer wasn't installed install all plugins
-	if packer_bootstrap then
-		require('packer').sync()
-	end
-end)
+-- set tab to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
---color settings
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
-vim.cmd.colorscheme("gruvbox")
-
---line numbers
+-- line numbering
 vim.opt.number = true
 vim.opt.relativenumber = true
 
---shift and tab width settings
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
+-- bufferline
+require('bufferline').setup{}
 
---lsp setup
-local lspconfig = require 'lspconfig'
-vim.g.coq_settings = { auto_start = 'shut-up' } --autostart coq for autocompletion w/ lspconfig
-local servers = {'eslint', 'html', 'cssls', 'jsonls'}
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup(require('coq').lsp_ensure_capabilities())
-end
+-- lualine
+require('lualine').setup {}
+
+-- gitsigns
+require('gitsigns').setup {}
+
+-- mini pairs
+require('mini.pairs').setup {}
+
+-- mini ai
+require('mini.ai').setup {}
